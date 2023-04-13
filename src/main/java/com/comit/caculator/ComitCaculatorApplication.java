f68ece3e2a789952aa743f73bee7e042c082448c
lihua <mrlu0208@gmail.com>
2023-04-12 20:19:06 -0600

    first commit

---
+++ src/main/java/com/comit/caculator/ComitCaculatorApplication.java
@@ -0,0 +1,13 @@
+package com.comit.caculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComitCaculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComitCaculatorApplication.class, args);
+	}
+
+}
